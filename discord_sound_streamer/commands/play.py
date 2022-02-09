b36c9f6ad824a867033a93a9c49f3e182855d345
zlalvani <zlalvani@gmail.com>
2022-02-08 21:48:14 -0500

    feat: add search and select commands

--- discord_sound_streamer/commands/play.py
+++ discord_sound_streamer/commands/play.py
@@ -1,6 +1,6 @@
 import tanjun
-from discord_sound_streamer.bot import bot
 from discord_sound_streamer.bot import lavalink
+from discord_sound_streamer.services import play as play_service
 
 component = tanjun.Component()
 
@@ -8,17 +8,12 @@ component = tanjun.Component()
 @tanjun.with_str_slash_option("name", "search term")
 @tanjun.as_slash_command("play", "play audio")
 async def play(ctx: tanjun.abc.Context, name: str) -> None:
-    guild = await ctx.fetch_guild()
-    if guild:
-        voice_state = guild.get_voice_state(ctx.author.id)
-        if voice_state:
-            await bot.update_voice_state(guild.id, voice_state.channel_id, self_deaf=True)
-            result = await lavalink.auto_search_tracks(name)
-            if result:
-                await lavalink.play(guild.id, result[0], ctx.author.id)
-                await ctx.respond(f'Playing {result[0].title}...')
-            else:
-                await ctx.respond(f'No results found for {name}...')
+    if ctx.guild_id:
+        result = await lavalink.auto_search_tracks(name)
+        if result: 
+            await play_service.play(ctx, result[0])
+        else:
+            await ctx.respond(f'No results found for {name}...')
 
 
 @component.with_slash_command
@@ -26,8 +21,11 @@ async def play(ctx: tanjun.abc.Context, name: str) -> None:
 async def skip(ctx: tanjun.abc.Context) -> None:
     if ctx.guild_id:
         queue = await lavalink.queue(ctx.guild_id)
-        await ctx.respond(f'Skipping {queue[0].title}...')
-        await lavalink.skip(ctx.guild_id)
+        if queue:
+            await ctx.respond(f'Skipping {queue[0].title}...')
+            if len(queue) > 1:
+                await ctx.respond(f'Playing {queue[1].title}...')
+            await lavalink.skip(ctx.guild_id)
 
 
 @component.with_slash_command
@@ -36,5 +34,17 @@ async def pause(ctx: tanjun.abc.Context) -> None:
     if ctx.guild_id:
         await ctx.respond(f'Pausing...')
         await lavalink.pause(ctx.guild_id)
+
+
+@component.with_slash_command
+@tanjun.as_slash_command("queue", "show the current queue")
+async def queue(ctx: tanjun.abc.Context) -> None:
+    if ctx.guild_id:
+        queue = await lavalink.queue(ctx.guild_id)
+        if queue:
+            await ctx.respond(f'Current queue: \n' + '\n'.join([t.title for t in queue]))
+        else:
+            await ctx.respond(f'Queue empty')
+
 
 loader = component.make_loader()
