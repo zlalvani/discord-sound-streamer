e9821d5067cf22b99434502b1894e467e3979782
zlalvani <zlalvani@gmail.com>
2022-02-11 02:01:04 -0500

    fix: lavaplayer.WS has unawaited exception

---
+++ discord_sound_streamer/websocket.py
@@ -0,0 +1,45 @@
+import logging
+
+import aiohttp
+from lavaplayer.websocket import _LOGGER
+from lavaplayer.websocket import WS as _WS
+
+
+class WS(_WS):
+
+    def __init__(
+        self,
+        ws: _WS,
+    ) -> None:
+        self.ws = ws.ws
+        self.ws_url = ws.ws_url
+        self.client = ws.client
+        self._headers = ws._headers
+        self._loop = ws._loop
+        self.emitter = ws.emitter
+        self.is_connect: bool = ws.is_connect
+    
+    async def _connect(self):
+        async with aiohttp.ClientSession(headers=self._headers, loop=self._loop) as session:
+            self.client.session = session
+            self.session = session
+            try:
+                self.ws = await self.session.ws_connect(self.ws_url)
+            except (aiohttp.ClientConnectorError, aiohttp.WSServerHandshakeError, aiohttp.ServerDisconnectedError, aiohttp.ClientConnectorError) as error:
+                _LOGGER.error(f"Could not connect to websocket: {error}")
+                return
+            _LOGGER.info("Connected to websocket")
+            self.is_connect = True
+            async for msg in self.ws:
+                if msg.type == aiohttp.WSMsgType.TEXT:
+                    try:
+                        await self.callback(msg.json())
+                    except Exception as e:
+                        _LOGGER.exception(e)
+                        continue
+                elif msg.type == aiohttp.WSMsgType.CLOSED:
+                    logging.error("close")
+                    break
+                elif msg.type == aiohttp.WSMsgType.ERROR:
+                    logging.error(msg.data)
+                    break
