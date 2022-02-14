1113110f00493784143faf5ab9f366d669319d86
Zaran Lalvani <zlalvani@gmail.com>
2022-02-14 02:22:27 -0500

    fix: handle requests from other voice channels (#11)

--- discord_sound_streamer/services/play.py
+++ discord_sound_streamer/services/play.py
@@ -2,9 +2,9 @@ from typing import List
 
 import tanjun
 from discord_sound_streamer.bot import bot, lavalink
+from discord_sound_streamer.config import CONFIG
 from hikari import Snowflake
 from lavaplayer import Track
-from discord_sound_streamer.logger import logger
 
 
 async def get_queue(guild_id: Snowflake) -> List[Track]:
@@ -17,11 +17,12 @@ async def play(ctx: tanjun.abc.Context, track: Track) -> None:
     guild = await ctx.fetch_guild()
     if guild:
         queue = await get_queue(guild.id)
-        if queue: 
-            await ctx.respond('Already a track playing in another channel.')
-            return
-        voice_state = guild.get_voice_state(ctx.author.id)
-        if voice_state:
-            await bot.update_voice_state(guild.id, voice_state.channel_id, self_deaf=True)
+        user_voice_state = guild.get_voice_state(ctx.author.id)
+        bot_voice_state = guild.get_voice_state(CONFIG.BOT_ID)
+        if user_voice_state:
+            if queue and bot_voice_state and bot_voice_state.channel_id != user_voice_state.channel_id: 
+                await ctx.respond('Already playing a track in another channel.')
+                return
+            await bot.update_voice_state(guild.id, user_voice_state.channel_id, self_deaf=True)
             await ctx.respond(f'Playing {track.title}...')
             await lavalink.play(guild.id, track, ctx.author.id)
