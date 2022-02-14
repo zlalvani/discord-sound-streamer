c54d471549ccbb8223c5c22281b95a8add036778
Zaran Lalvani <zlalvani@gmail.com>
2022-02-13 23:50:23 -0500

    fix: handle exceptions when commands are run while not in voice (#10)

--- discord_sound_streamer/services/play.py
+++ discord_sound_streamer/services/play.py
@@ -1,12 +1,25 @@
-import tanjun
-from lavaplayer import Track
+from typing import List
 
-from discord_sound_streamer.bot import bot
-from discord_sound_streamer.bot import lavalink
+import tanjun
+from discord_sound_streamer.bot import bot, lavalink
+from hikari import Snowflake
+from lavaplayer import Track
+from discord_sound_streamer.logger import logger
+
+
+async def get_queue(guild_id: Snowflake) -> List[Track]:
+    if await lavalink.get_guild_node(guild_id):
+        return await lavalink.queue(guild_id)    
+    return []
+
 
 async def play(ctx: tanjun.abc.Context, track: Track) -> None:
     guild = await ctx.fetch_guild()
     if guild:
+        queue = await get_queue(guild.id)
+        if queue: 
+            await ctx.respond('Already a track playing in another channel.')
+            return
         voice_state = guild.get_voice_state(ctx.author.id)
         if voice_state:
             await bot.update_voice_state(guild.id, voice_state.channel_id, self_deaf=True)
