c48a76b354bca364876fbb82a922f50e0bfc1ece
zlalvani <zlalvani@gmail.com>
2022-03-05 01:22:29 -0500

    fix: selecting and searching no longer closes search early

--- discord_sound_streamer/datastore/models/search.py
+++ discord_sound_streamer/datastore/models/search.py
@@ -1,12 +1,15 @@
 from dataclasses import dataclass
+from datetime import datetime
 from typing import List
 
 from hikari import Snowflake
 from lavaplayer import Track
 
+
 @dataclass
 class SearchWaitValue:
     tracks: List[Track]
+    searched_at: datetime
 
 # frozen and eq make the class hashable
 @dataclass(frozen=True, eq=True)
