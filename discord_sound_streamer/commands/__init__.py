6dc236032f0152d4214e51a406c17b927512774d
zlalvani <zlalvani@gmail.com>
2022-08-21 22:05:05 -0400

    feat: add error handler, invidious retries

--- discord_sound_streamer/commands/__init__.py
+++ discord_sound_streamer/commands/__init__.py
@@ -5,7 +5,12 @@ from discord_sound_streamer.services import commands as commands_service
 
 component = tanjun.Component()
 
-hooks: tanjun.Hooks = tanjun.Hooks().add_on_success(commands_service.update_last_command_time)
+hooks: tanjun.Hooks = (
+    tanjun.Hooks()
+    .add_on_success(commands_service.update_last_command_time)
+    .add_on_error(commands_service.command_error_handler)
+)
+
 
 client.set_slash_hooks(hooks)
 
