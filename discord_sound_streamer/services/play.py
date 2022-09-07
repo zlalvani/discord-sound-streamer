52b64d9c2cf22e3f2f54b3a436f6da4b04737231
zlalvani <zlalvani@gmail.com>
2022-09-07 01:53:59 -0400

    feat: use pydantic BaseSettings

--- discord_sound_streamer/services/play.py
+++ discord_sound_streamer/services/play.py
@@ -40,34 +40,35 @@ async def _play_tracks(ctx: tanjun.abc.Context, guild: Guild, tracks: List[Track
     queue = await get_queue(guild.id)
     user_voice_state = guild.get_voice_state(ctx.author.id)
     bot_voice_state = guild.get_voice_state(CONFIG.BOT_ID)
-    if user_voice_state:
-        if queue and bot_voice_state and bot_voice_state.channel_id != user_voice_state.channel_id:
-            await embed_service.reply_message(ctx, "Already playing a track in another channel.")
-            return
 
-        await bot.update_voice_state(guild.id, user_voice_state.channel_id, self_deaf=True)
-
-        if len(tracks) == 1:
-            await ctx.respond(embed=embed_service.build_track_embed(tracks[0], title="Queueing..."))
-        else:
-            await ctx.respond(embed=embed_service.build_playlist_embed(tracks))
-
-        for track in tracks:
-            # There can be a race condition where the bot hasn't yet joined the voice channel
-            # before attempting to play. In that case, we retry.
-            try:
-                async for attempt in AsyncRetrying(
-                    retry=retry_if_exception_type(NodeError),
-                    stop=stop_after_attempt(3),
-                    wait=wait_fixed(0.25),
-                ):
-                    with attempt:
-                        await lavalink.play(guild.id, track, ctx.author.id)
-            except RetryError:
-                pass
-
-    else:
+    if not user_voice_state:
         await embed_service.reply_message(ctx, "You are not in a voice channel!")
+        return
+
+    if queue and bot_voice_state and bot_voice_state.channel_id != user_voice_state.channel_id:
+        await embed_service.reply_message(ctx, "Already playing a track in another channel.")
+        return
+
+    await bot.update_voice_state(guild.id, user_voice_state.channel_id, self_deaf=True)
+
+    if len(tracks) == 1:
+        await ctx.respond(embed=embed_service.build_track_embed(tracks[0], title="Queueing..."))
+    else:
+        await ctx.respond(embed=embed_service.build_playlist_embed(tracks))
+
+    for track in tracks:
+        # There can be a race condition where the bot hasn't yet joined the voice channel
+        # before attempting to play. In that case, we retry.
+        try:
+            async for attempt in AsyncRetrying(
+                retry=retry_if_exception_type(NodeError),
+                stop=stop_after_attempt(3),
+                wait=wait_fixed(0.25),
+            ):
+                with attempt:
+                    await lavalink.play(guild.id, track, ctx.author.id)
+        except RetryError:
+            pass
 
 
 async def pause_control(ctx: tanjun.abc.Context, pause: bool) -> None:
