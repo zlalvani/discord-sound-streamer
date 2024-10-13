0ad30c98462ac20b1a22f61478984d5a89ed531a
zlalvani <zlalvani@gmail.com>
2024-10-13 04:18:36 -0400

    feat: /current has player controls

--- discord_sound_streamer/services/interaction.py
+++ discord_sound_streamer/services/interaction.py
@@ -1,10 +1,10 @@
-from lavalink import AudioTrack
+from lavalink import AudioTrack, DefaultPlayer
 from enum import Enum
 
 from hikari.impl.special_endpoints import (
     MessageActionRowBuilder,
 )
-from hikari import ComponentInteraction, ComponentType
+from hikari import ComponentInteraction, ComponentType, ButtonStyle, ResponseType
 
 from discord_sound_streamer.datastore.models.search import SearchWaitKey
 from discord_sound_streamer.logger import logger
@@ -12,10 +12,17 @@ from discord_sound_streamer.datastore.operations import search as search_operati
 from discord_sound_streamer.polymorphs.responder import InteractionResponder
 from discord_sound_streamer.services import youtube as youtube_service
 from discord_sound_streamer.services import play as play_service
+from discord_sound_streamer.services import embed as embed_service
 
 
 class InteractionMode(str, Enum):
     SEARCH_SELECT = "search_select"
+    SKIP = "controls:skip"
+    REFRESH = "controls:refresh"
+    SEEK_BACK_BIG = "controls:seek_back_big"
+    SEEK_BACK_SMALL = "controls:seek_back_small"
+    SEEK_FORWARD_SMALL = "controls:seek_forward_small"
+    SEEK_FORWARD_BIG = "controls:seek_forward_big"
 
 
 def build_search_interaction(search_results: list[AudioTrack]):
@@ -34,6 +41,40 @@ def build_search_interaction(search_results: list[AudioTrack]):
     return [row]
 
 
+def build_current_controls_interaction(player: DefaultPlayer):
+    commands_row = MessageActionRowBuilder()
+    seek_row = MessageActionRowBuilder()
+
+    progress_max_length = 32
+    if player.current:
+        progress = player.position / player.current.duration
+        progress = int(progress * progress_max_length)
+    else:
+        progress = 0
+
+    commands_row.add_interactive_button(
+        ButtonStyle.PRIMARY,
+        InteractionMode.REFRESH,
+        label="█" * progress + "░" * (progress_max_length - progress),
+    )
+
+    seek_row.add_interactive_button(
+        ButtonStyle.SECONDARY, InteractionMode.SEEK_BACK_BIG, label="<<"
+    )
+    seek_row.add_interactive_button(
+        ButtonStyle.SECONDARY, InteractionMode.SEEK_BACK_SMALL, label="<"
+    )
+    seek_row.add_interactive_button(ButtonStyle.DANGER, InteractionMode.SKIP, emoji="⏭️")
+    seek_row.add_interactive_button(
+        ButtonStyle.SECONDARY, InteractionMode.SEEK_FORWARD_SMALL, label=">"
+    )
+    seek_row.add_interactive_button(
+        ButtonStyle.SECONDARY, InteractionMode.SEEK_FORWARD_BIG, label=">>"
+    )
+
+    return [commands_row, seek_row]  # , commands_row]
+
+
 async def handle_component_interaction(interaction: ComponentInteraction):
     if interaction.component_type == ComponentType.TEXT_SELECT_MENU:
         mode = interaction.custom_id
@@ -42,8 +83,79 @@ async def handle_component_interaction(interaction: ComponentInteraction):
             await _search_select(interaction, selected)
         else:
             logger.warning(f"Unknown interaction mode: {mode}")
+    elif interaction.component_type == ComponentType.BUTTON:
+        mode = interaction.custom_id
+        if mode == InteractionMode.REFRESH:
+            await _refresh_controls(interaction)
+        elif mode == InteractionMode.SKIP:
+            await _skip(interaction)
+        elif mode == InteractionMode.SEEK_BACK_BIG:
+            await _seek(interaction, -30000)
+        elif mode == InteractionMode.SEEK_BACK_SMALL:
+            await _seek(interaction, -5000)
+        elif mode == InteractionMode.SEEK_FORWARD_BIG:
+            await _seek(interaction, 30000)
+        elif mode == InteractionMode.SEEK_FORWARD_SMALL:
+            await _seek(interaction, 5000)
+        else:
+            logger.warning(f"Unknown interaction mode: {mode}")
     else:
         logger.warning(f"Unknown component type: {interaction.component_type}")
+
+
+async def _refresh_controls(interaction: ComponentInteraction, initial: bool = True):
+    if not interaction.guild_id:
+        return
+
+    player = play_service.get_player(interaction.guild_id)
+    if not player.current:
+        return
+
+    if initial:
+        await interaction.create_initial_response(
+            ResponseType.MESSAGE_UPDATE,
+            embed=embed_service.build_track_embed(
+                player.current, current_position=player.position
+            ),
+            components=build_current_controls_interaction(player),
+        )
+    else:
+        await interaction.edit_message(
+            interaction.message.id,
+            embeds=[
+                embed_service.build_track_embed(
+                    player.current, current_position=player.position
+                )
+            ],
+            components=build_current_controls_interaction(player),
+        )
+
+
+async def _skip(interaction: ComponentInteraction):
+    if not interaction.guild_id:
+        return
+
+    player = play_service.get_player(interaction.guild_id)
+    if not player.current:
+        return
+
+    responder = InteractionResponder(interaction)
+
+    await responder.respond_message(f"Skipping {player.current.title}...")
+    await player.skip()
+    await _refresh_controls(interaction, initial=False)
+
+
+async def _seek(interaction: ComponentInteraction, offset: int):
+    if not interaction.guild_id:
+        return
+
+    player = play_service.get_player(interaction.guild_id)
+    if not player.current:
+        return
+
+    await player.seek(max(0, min(player.position + offset, player.current.duration)))
+    await _refresh_controls(interaction)
 
 
 async def _search_select(interaction: ComponentInteraction, selected: int):
